ad44608d42396fe7aa8765c5b7313d225fab2b8f
mrzhangboss <2529450174@qq.com>
2016-09-26 10:17:23 +0800

    refator framework

---
+++ doctor/Managers.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+
+
+class DBManage:
+    def __init__(self, filename):
+        pass
+
+    def select(self, keyword):
+        pass
+
+    def update(self, keyword):
+        return False
+
+    def insert(self, keyword, data):
+        return False
+
+    def delete(self, keyword):
+        return False
+
+#todo: set apart from every module
+class Manage:
+    pass
---
+++ doctor/Spider.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+import json
+import requests
+from .Managers import DBManage
+
+
+class Spider:
+    youdao_api = 'http://fanyi.youdao.com/openapi.do?keyfrom={keyfrom}&key={key}&type=data&doctype=json&version=1.1&q={keyword}'
+
+    def __init__(self, keyword, filename='', keyfrom='EnglishSearcher', key='1098649915'):
+        self.keyword = keyword
+        self.content = None
+        self.db = DBManage(filename)
+        self.keyfrom = keyfrom
+        self.key = key
+
+    def search(self):
+        url = self.youdao_api.format(keyfrom=self.keyfrom, key=self.key, keyword=self.keyword)
+        response = requests.get(url)
+        self.content = response.json()
+
+    def find(self):
+        self.db.select(self.keyword)
+        self.content = 'find'
+
+
+    @property
+    def result(self):
+        self.find()
+        self.search()
+        return self
